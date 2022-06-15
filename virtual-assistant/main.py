2060fd2c6e31bcdf46fb118b6e30c7b779240423
quenn <75434427+amalie11@users.noreply.github.com>
2022-06-15 21:18:17 +0300

    Add files via upload

---
+++ virtual-assistant/main.py
@@ -0,0 +1,52 @@
+# a very basic virtual assistant
+#so i'll install speechRecognition,pyttx3(to speak),and wikipedia to get info from it
+
+import speech_recognition as sr
+import pyttsx3 as pyt
+import wikipedia
+import datetime
+
+
+listen = sr.Recognizer()
+engine = pyt.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listen.listen(source)
+            command = listen.recognize_google(voice)
+            command = command.lower()
+            if 'jarvis' in command:
+                command = command.replace('jarvis', '')
+                print(command)
+    except:
+        pass
+    return command
+
+def run_alexa():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing ' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        talk('Current time is ' + time)
+    elif 'who is' in command:
+        person = command.replace('who is', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    else:
+        talk('Sorry I did not quite get what yu just said.')
+
+while True:
+    run_alexa()
